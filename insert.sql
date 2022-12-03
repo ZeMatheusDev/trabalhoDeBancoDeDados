61293ce45fa80bab96d864936f16870bee8576d4
Matheus <teteulopes2011@hotmail.com>
2022-12-03 15:58:48 -0300

    Adição do update e adição de linhas no insert

--- insert.sql
+++ insert.sql
@@ -23,6 +23,16 @@ INSERT INTO clientes
 values
 ('Clodovaldo', '025145854125', '3218455', '4521521', 'Rua tereza de vilela');
 
+INSERT INTO clientes
+(nome, cpf, login, senha, endereco)
+values
+('Rironildo', '18444558747', '1251518', '12114561', 'Rua amado batista');
+
+INSERT INTO clientes
+(nome, cpf, login, senha, endereco)
+values
+('Robson', '05148454526', '43782147', '124382841', 'Rua clodovildo amado');
+
 INSERT INTO produtos
 (nome, descricao, valor,marca,qtd_estoque)
 values
@@ -52,6 +62,18 @@ INSERT INTO produtos
 values
 ('Calça jeans', 'calça jeans numero 32', '$220,00','Mister',5);
 
+INSERT INTO produtos
+(nome, descricao, valor,marca,qtd_estoque)
+
+values
+('Moletom', 'Moletom de frio M', '$150,00','Mister',8);
+
+INSERT INTO produtos
+(nome, descricao, valor,marca,qtd_estoque)
+
+values
+('Sapato social', 'Sapato social numero 41', '$110,00','xinghanghu',15);
+
 INSERT INTO clientes_produtos(usuario_id, produto_id)
 SELECT c.id, p.id FROM clientes c, produtos p WHERE c.id = 1 && p.id = 1
  LIMIT 1;
@@ -70,6 +92,14 @@ SELECT c.id, p.id FROM clientes c, produtos p WHERE c.id = 3 && p.id = 5
  
  INSERT INTO clientes_produtos(usuario_id, produto_id)
 SELECT c.id, p.id FROM clientes c, produtos p WHERE c.id = 5 && p.id = 4
+ LIMIT 1;
+
+  INSERT INTO clientes_produtos(usuario_id, produto_id)
+SELECT c.id, p.id FROM clientes c, produtos p WHERE c.id = 6 && p.id = 7
+ LIMIT 1;
+
+  INSERT INTO clientes_produtos(usuario_id, produto_id)
+SELECT c.id, p.id FROM clientes c, produtos p WHERE c.id = 7 && p.id = 6
  LIMIT 1;
 
 
---
+++ update.sql
@@ -0,0 +1,8 @@
+UPDATE clientes SET nome = 'Ednaldo Pereira' WHERE id = 1; -- Update no nome DO CLIENTE no cliente de id 1
+UPDATE clientes SET nome = 'Jurisvaldo' WHERE id = 2;
+
+UPDATE produtos SET qtd_estoque = 30 WHERE id = 4; -- Update na quantidade de produtos na tabela produto de id 4
+UPDATE produtos SET qtd_estoque = 25 WHERE id = 5;
+
+UPDATE clientes_produtos SET usuario_id = 5 WHERE id = 1; -- Update no id do usuario relacionando a compra do produto
+UPDATE clientes_produtos SET produto_id = 6 WHERE id = 2; -- Update no id do produto relacionando ao comprador do produto
