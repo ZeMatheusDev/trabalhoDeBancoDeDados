e187632a9005926083f2ce9b228c6640f3aedaae
Matheus <teteulopes2011@hotmail.com>
2022-12-03 12:53:33 -0300

    primeiro commit

---
+++ create.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE loja;
+USE loja;
+
+CREATE TABLE `clientes` (
+	id INT AUTO_INCREMENt,
+    nome varchar(50),
+    cpf varchar(50),
+    login varchar(50),
+    senha varchar(50),
+    endereco varchar(50),
+    
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `produtos` (
+  	id INT AUTO_INCREMENT,
+    nome varchar(50),
+    descricao varchar(50),
+    valor varchar(50),
+    
+    PRIMARY KEY (`id`)
+
+);
+
+CREATE TABLE `clientes_produtos` (
+  	id INT AUTO_INCREMENT,
+  	usuario_id INT,
+  	produto_id INT,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fk_clientes` FOREIGN KEY(usuario_id) REFERENCES clientes (id), 
+    CONSTRAINT `fk_produtos` FOREIGN KEY (produto_id) REFERENCES produtos(id)
+);
