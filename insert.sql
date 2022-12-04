af382c4041fe6a8261c5449e5c96b46f00370dd2
Cayo Capuxy <cayocapuxy@hotmail.com.br>
2022-12-04 10:38:16 -0300

    Formatação

--- insert.sql
+++ insert.sql
@@ -35,42 +35,42 @@ values
 ('Robson', '05148454526', '43782147', '124382841', 'Rua clodovildo amado');
 
 INSERT INTO produtos
-(nome, descricao, valor,marca,qtd_estoque)
+(nome, descricao, valor, marca, qtd_estoque)
 values
 ('Relogio', 'relogio de pulso dourado', '$150,00','Mormaii',2);
 
 INSERT INTO produtos
-(nome, descricao, valor,marca,qtd_estoque)
+(nome, descricao, valor, marca, qtd_estoque)
 
 values
 ('Chapeu', 'chapeu aba reta preto', '$70,00','Rexpeita',3);
 
 INSERT INTO produtos
-(nome, descricao, valor,marca,qtd_estoque)
+(nome, descricao, valor, marca, qtd_estoque)
 
 values
 ('Camisa de manga', 'camisa social de manga M', '$120,00','Hering',4);
 
 INSERT INTO produtos
-(nome, descricao, valor,marca,qtd_estoque)
+(nome, descricao, valor, marca, qtd_estoque)
 
 values
 ('Bermuda', 'bermuda de praia numero 32', '$90,00','Mister',5);
 
 INSERT INTO produtos
-(nome, descricao, valor,marca,qtd_estoque)
+(nome, descricao, valor, marca, qtd_estoque)
 
 values
 ('Calça jeans', 'calça jeans numero 32', '$220,00','Mister',5);
 
 INSERT INTO produtos
-(nome, descricao, valor,marca,qtd_estoque)
+(nome, descricao, valor ,marca, qtd_estoque)
 
 values
 ('Moletom', 'Moletom de frio M', '$150,00','Mister',8);
 
 INSERT INTO produtos
-(nome, descricao, valor,marca,qtd_estoque)
+(nome, descricao, valor, marca, qtd_estoque)
 
 values
 ('Sapato social', 'Sapato social numero 41', '$110,00','xinghanghu',15);
